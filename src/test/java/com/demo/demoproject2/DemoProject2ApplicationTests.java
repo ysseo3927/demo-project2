826d16a44ff296b950f8aaa3aad9984ec7ac2173
ysseo3927 <ysseo3927@gmail.com>
2023-03-15 16:57:09 +0900

    Initial commit

---
+++ src/test/java/com/demo/demoproject2/DemoProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demoproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
